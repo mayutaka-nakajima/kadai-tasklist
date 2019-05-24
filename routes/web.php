cd93af3b89d44c357e5fa4d998539fb1e7b91f95
mayutaka.nakajima <mayutaka-nakajima@dac.co.jp>
2019-05-24 02:24:46 +0000

    change toppage and add user registration

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', "TasksController@index");
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource("tasks", "TasksController");
+
+
+
+//ユーザ関連
+
+//ユーザ登録
+Route::get("signup", "Auth\RegisterController@showRegistrationForm")->name("signup.get");
+Route::post("signup", "Auth\RegisterController@register")->name("signup.post");
